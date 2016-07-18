a64d69bcdcc5f1cc7fc5ebc18c440844014c315f
Henry <th.saravana@gmail.com>
2016-07-18 14:27:06 +0530

    Removing parts of the style to see how it looks in the mobile ui
    - adding JS control to download fonts

---
+++ dev/postwrap/fonts/js/android.js
@@ -0,0 +1,4 @@
+function downloadUrl() {
+    var fontUrlArray = new Array("https://fonts.google.com/download?family=Galada|Poiret%20One|Bangers|Monoton|Finger%20Paint|Love%20Ya%20Like%20A%20Sister|Caesar%20Dressing|Fredericka%20the%20Great");
+    Android.download(fontUrlArray, "Display Fonts");
+}
