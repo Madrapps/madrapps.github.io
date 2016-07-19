2dec8447575a6aa1ce8c235ae6df48dd39dc4879
Henry <th.saravana@gmail.com>
2016-07-19 13:05:58 +0530

    1. changed UI 2. Testing if we could directly download .ttf file, instead of zip file

--- dev/postwrap/fonts/js/android.js
+++ dev/postwrap/fonts/js/android.js
@@ -1,11 +1,11 @@
 function downloadDisplayFonts() {
     var fontUrlArray = new Array("https://fonts.google.com/download?family=Galada|Poiret%20One|Bangers|Monoton|Finger%20Paint|Love%20Ya%20Like%20A%20Sister|Caesar%20Dressing|Fredericka%20the%20Great");
-    Android.download(fontUrlArray, "Display Fonts");
+    Android.download(fontUrlArray, "Display Fonts","fonts.zip");
 }
 
 function downloadHandwritingFonts() {
     var fontUrlArray = new Array("https://fonts.google.com/download?family=Pacifico|Amatic%20SC|Dancing%20Script|Courgette|Satisfy|Sacramento|Merienda%20One|Clicker%20Script");
-    Android.download(fontUrlArray, "Handwriting Fonts");
+    Android.download(fontUrlArray, "Handwriting Fonts", "fonts.zip");
 }
 
 function downloadLanguageFonts() {
@@ -22,5 +22,12 @@ function downloadLanguageFonts() {
         "https://fonts.google.com/download?family=Lalezar",
         "https://fonts.google.com/download?family=Katibeh"
     ];
-    Android.download(fontUrlArray, "Language Fonts");
+    Android.download(fontUrlArray, "Language Fonts", "fonts.zip");
+}
+
+function downloadTTFFont(){
+    var fontUrlArray = [
+        "http://www5.miele.nl/apps/vg/nl/miele/mielea02.nsf/0e87ea0c369c2704c12568ac005c1831/07583f73269e053ac1257274003344e0/$FILE/arial.ttf"
+    ];
+    Android.download(fontUrlArray, "Language Fonts", "arial.ttf");
 }
